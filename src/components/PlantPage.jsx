e45d5bd8837776156c0d95a92341b53905928f51
dickens <dickens@gmail.com>
2026-05-11 16:18:34 +0300

    Finalize Task 4: Documentation and cleanup

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
 import Search from "./Search";
+import PlantList from "./PlantList";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={setPlants} plants={plants} />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
+      <PlantList plants={plants} searchTerm={searchTerm} />
     </main>
   );
 }
